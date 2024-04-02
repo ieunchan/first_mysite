b911320e4b9537df5d91935a68af8b74e95cbe9a
eunchan lee <jsmith26@gmail.com>
2024-04-02 19:28:16 +0900

    update

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from .import views
 
+app_name = "polls"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("coffee", views.coffee, name = "coffee"),
-    path("kfood", views.Korean_food, name="koreanfood")
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
 
+
+app_name = "polls"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from .models import Choice, Question
 
-def index(request):
-    return HttpResponse("안녕하세요. 실습중입니다.")
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def coffee(request):
-    return HttpResponse("아메리카노, 에스프레소, 카페라떼, 초코라떼, 카푸치노, 모카, 콜드브루, 드립커피, 마끼아또, 아인슈페너, 프렌치 프레스, 콘 파냐, 아이리쉬 커피")
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def Korean_food(request):
-    return HttpResponse("비빔밥, 간장게장, 불고기, 국밥, 제육볶음, 김치, 삼겹살, 야채곱창")
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
